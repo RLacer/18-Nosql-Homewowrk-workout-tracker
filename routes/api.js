3b95cd32baf868766ae3a66046247ad8af1cb241
RLacer <r.a.lacer@hotmail.com>
2021-06-09 12:59:22 -0600

    working on routes

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,82 @@
 const router = require("express").Router();
+const { findByIdAndDelete } = require("../models/workout");
 const Workout = require("../models/workout");
 
 router.post("/api/workouts", (req, res) => {
-    Workout.create({ })
-    .then((dbworkout, ))
+    Workout.create({})
+        .then((dbworkout) => {
+            res.json(dbworkout)
+        })
+        .catch(err => {
+            res.json(err)
+        })
 })
+
+router.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+        params.id,
+        {
+            $push: { exercise: body }
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+    )
+    .then((dbworkout) => {
+        res.json(dbworkout)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+});
+
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+    .then((dbworkouts) => {
+        res.json(dbworkouts)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum: "$exercises.duration"
+                }
+            }
+        }
+    ])
+    .sort({ _id: -1 }) 
+    .limit(5)
+    .then((dbworkouts) => {
+        res.json(dbworkouts)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+router.delete("/api/workouts", ({ body }, res) => {
+    Workout.findByIdAndDelete(body.id)
+    .then(() => {
+        res.json(true)
+    })
+    .catch(err => {
+        res.json(err)
+    })
+})
+
+module.exports = router;
