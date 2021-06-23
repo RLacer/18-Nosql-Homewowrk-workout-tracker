1224999fde6c117d1c4b28b9f1bd2de14b3d1902
RLacer <r.a.lacer@hotmail.com>
2021-06-23 13:29:48 -0600

    added routes

--- server.js
+++ server.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 
+const app = express();
 const PORT = 3000;
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-app.use(routes);
+
 
 mongoose.connect("mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
+
+app.use(require("./routes/api.js"));
+app.use(require("./routes/view.js"));
 
 app.listen(PORT, () => {console.log("app running on port on 3000")});
 
