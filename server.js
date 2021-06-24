f1fe35c8ab5df11630f3e223d2a6163d759f342c
RLacer <r.a.lacer@hotmail.com>
2021-06-24 11:18:15 -0600

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,14 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(
+    process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify: false
-});
+}
+);
 
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
